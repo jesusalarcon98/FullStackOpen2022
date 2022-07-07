c0420126b8e9101fbf4b444c60dafa340b4c9d67
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2022-07-06 20:11:35 -0500

    Part 1.6 Unicafe

--- src/index.js
+++ src/index.js
@@ -1,64 +1,38 @@
-import React from "react"; //funciona todo bien de momento
+import React, { useState } from "react"; //importamos la funcion state
 import ReactDOM from "react-dom";
 
-const Part = (props) => {
-  //Constante que hace que al componente CONTENT solo se le pase 1 ejercicio y ahí pueda hacerlo con los 3
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  );
-};
-const Header = (props) => {
-  //constante que hace que el componentente HEAD se le pase el curso
-  return <h1> {props.course}</h1>;
-};
-const Content = (props) => {
-  //Constante que habla al componente PART el cual se usa 1 solo componente 3 veces
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  );
-};
-const Total = (props) => {
-  //Constante que hace que el componente tome los 3 ejercicios y los suma.
-  return (
-    <p>
-      Number of exercises:{" "}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </p>
-  );
-};
+const Title = ({ text }) => <h1>{text}</h1>;
 
+const Button = ({ handleClick, text, value }) => (
+  <button onClick={handleClick}>
+    {text} {value}
+  </button>
+);
+
+const Text = ({ text, value }) => (
+  <div>
+    {text} {value}
+  </div>
+);
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <div>
+        <Title text="Give feedbacks" />
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      </div>
+      <div>
+        <Title text="Statics" />
+        <Text text="good" value={good} />
+        <Text text="neutral" value={neutral} />
+        <Text text="bad" value={bad} />
+      </div>
     </div>
   );
 };
