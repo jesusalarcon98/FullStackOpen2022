f59bc987c2e5e3ed2cc431995114e76bff50a8b7
jesusalarcon98 <jalarcon@isavanzados.com.mx>
2022-08-01 17:41:52 -0500

    Creando anecdotas y comprando ejercicios y comprobando unicafe

---
+++ Part1/anecdotes/src/index.js
@@ -0,0 +1,12 @@
+import React from "react"; //funciona todo bien de momento
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <p>Hola mundo</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
