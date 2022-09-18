3ee8e25746f07b7734181249dc9bfcbf1f7565dd
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2022-09-17 21:58:34 -0500

    Ejercicio 2.11. Phonebook con UseEffect

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import Form from "./components/Form.js";
 import InputFilter from "./components/InputFilter";
+import axios from "axios";
 
+/* 
+
+const App = () => {
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  };
+  console.log("render", notas.length, "notes");
+  useEffect(hook, []); */
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [personsFilter, setPersonsFilter] = useState("");
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promesa");
+      setPersons(response.data);
+    });
+  };
+  console.log("render", persons.length, "persons");
+  useEffect(hook, []);
 
   const addPersons = (e) => {
     e.preventDefault();
