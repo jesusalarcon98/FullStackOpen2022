4bfcab896124beca289463e0f1f439bce1e2730a
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2022-09-22 22:05:27 -0500

    prueba 2.12

--- Part2/countries/src/components/Filter.js
+++ Part2/countries/src/components/Filter.js
@@ -1,19 +1,29 @@
 import Countrie from "./Countrie";
 
 const Filter = ({ countries, countriesFilter }) => {
-  return countries
-    .filter((val) => {
-      if (countriesFilter === "") {
-        return val;
-      } else if (
-        val.name.common.toLowerCase().includes(countriesFilter.toLowerCase())
-      ) {
-        return val;
-      }
-    })
-    .map((countrie, index) => {
-      return <Countrie index={index} countrie={countrie} />;
-    });
+  if (countriesFilter === "") {
+    return (
+      <div>
+        <p>Too many matches, specify another filter</p>
+      </div>
+    );
+  } else if (countriesFilter !== "") {
+    return countries
+      .filter((val) => {
+        console.log(
+          val.name.common.toLowerCase().includes(countriesFilter.toLowerCase())
+            .length
+        );
+        if (
+          val.name.common.toLowerCase().includes(countriesFilter.toLowerCase())
+        ) {
+          return val;
+        }
+      })
+      .map((countrie, index) => {
+        return <Countrie index={index} countrie={countrie} />;
+      });
+  }
 };
 
 export default Filter;
