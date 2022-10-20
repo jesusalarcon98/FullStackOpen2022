e0d3bab066912cab8e72a03610462396c6b41405
jesusalarcon98 <jalarcon@isavanzados.com.mx>
2022-10-20 18:04:04 -0500

    Starting 2.e

--- Part2/exercises/src/index.js
+++ Part2/exercises/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import './index.css'
 
 import axios from "axios";
 
 axios.get("http://localhost:3001/notes").then((response) => {
   const notes = response.data;
-  ReactDOM.render(<App notes={notes} />, document.getElementById("root"));
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <App notes={notes} />
+    </React.StrictMode>
+  );
 });
+/*  */
