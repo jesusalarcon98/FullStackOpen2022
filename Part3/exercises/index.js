2dac099f5ffbf9d1614aa64c43c0a8e597adea0e
jesusalarcon98 <jalarcon@isavanzados.com.mx>
2022-10-25 10:58:35 -0500

    Complete exercises part a

--- Part3/exercises/index.js
+++ Part3/exercises/index.js
@@ -1,6 +1,6 @@
-const { response } = require("express");
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let notes = [
   {
@@ -47,6 +47,29 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
+});
+
+const generateId = () => {
+  const maxID = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxID + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  response.json(note);
 });
 
 const PORT = 3001;
