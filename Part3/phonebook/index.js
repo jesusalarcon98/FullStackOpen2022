13fb375c9085830021b5f58439288b04b36a4190
jesusalarcon98 <jalarcon@isavanzados.com.mx>
2022-10-25 18:01:24 -0500

    Part 3 Exercises 3.1 - 3.6 Complete

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -1,5 +1,8 @@
+const { request, response } = require("express");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 let persons = [
   {
@@ -32,6 +35,58 @@ let persons = [
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
+
+app.get("/api/info", (request, response) => {
+  const maxID = persons.length;
+  const dates = new Date().toString();
+  response.send(`<p>Phonebook has info for ${maxID} people</p>
+  <p>${dates}</p>`);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(400).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+const IDRandom = () => {
+  const numberID = Math.floor(Math.random() * 100000);
+  return numberID;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  const nameUnique = persons.find((person) => person.name === body.name);
+  const checkExists = !body.name || !body.number;
+  if (checkExists) {
+    return response.status(400).json({
+      error: "missing name or number",
+    });
+  }
+  if (nameUnique) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: IDRandom(),
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
