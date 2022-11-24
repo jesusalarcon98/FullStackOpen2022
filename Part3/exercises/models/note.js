05e6ea96f83ab3ed4e47231f17bab052e40aa60b
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2022-11-23 18:02:07 -0600

    Valicoon y eslint

--- Part3/exercises/models/note.js
+++ Part3/exercises/models/note.js
@@ -13,12 +13,18 @@ mongoose
     console.log("error connecting to MongoDB:", error.message);
   });
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-});
-
+  const noteSchema = new mongoose.Schema({
+    content: {
+      type: String,
+      minlength: 5,
+      required: true
+    },
+    date: { 
+      type: Date,
+      required: true
+    },
+    important: Boolean
+  })
 noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
