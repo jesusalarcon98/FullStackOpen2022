484d28dd5f64d6dc64a61c9690a3005fe7a8c365
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2022-12-22 13:41:39 -0600

    Part 4.1 complete

---
+++ Part4/bloglist/models/bloglist.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+const url = process.env.MONGODB_URL;
+console.log("connected to", url);
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting:", error.message);
+  });
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
